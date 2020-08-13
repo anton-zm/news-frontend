745fd64ee9117eaa6278a121819111edc32ffad0
Anton Zmanovskii <antonzm34@gmail.com>
2020-08-13 16:12:36 +0400

    Вёрстка основных секций главной

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+
+const authBtn = document.querySelector('.header__button');
+const copyrigth = document.querySelector('.footer__copyright');
+
+copyrigth.textContent = `© ${new Date().getFullYear()} Supersite, Powered by News API`
