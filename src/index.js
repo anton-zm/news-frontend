fde5617cb21fe419796c3d9cdbc533183992f544
Anton Zmanovskii <antonzm34@gmail.com>
2020-08-15 13:04:55 +0400

    вёрстка попапов

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import './style.css';
 
+import Popup from "./js/Popup";
+
+
 const authBtn = document.querySelector('.header__button');
 const copyrigth = document.querySelector('.footer__copyright');
 const techContainer = document.querySelector('.tech');
+const signupButton = document.querySelector('#signup-btn');
+const signinForm = document.querySelector('#sign-in-popup');
+const signupForm = document.querySelector('#sign-up-popup');
+const success = document.querySelector('#success-popup');
+const signupLink = document.querySelector('#popup__signup-link');
 
+
+
+const signinPopup = new Popup(signinForm);
+const signupPopup = new Popup(signupForm);
+const successPopup = new Popup(success);
+
+// меняет "Ничего не найдено на лоадер"
 
 techContainer.addEventListener('click', (e) => {
 
@@ -13,6 +28,43 @@ techContainer.addEventListener('click', (e) => {
         <h3 class="tech__title">Ничего не найдено</h3>
         <p class="tech__message">К сожалению по вашему запросу ничего не найдено.</p>`
     }, 3000)
+})
+
+// открывают попап входа
+authBtn.addEventListener('click', () => {
+    signinPopup.open();
+})
+
+window.addEventListener('click', (event) => {
+    if (event.target.classList.contains('popup__link_to-signin')) {
+        signinPopup.open();
+        signupPopup.close();
+        successPopup.close();
+    }
+})
+
+// открывает попап регистрации
+
+signupLink.addEventListener('click', () => {
+    signupPopup.open();
+    signinPopup.close();
+})
+
+// открывает попап успешной регистрации
+signupButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    successPopup.open();
+    signupPopup.close();
+
+})
+
+// закрывает попап
+window.addEventListener('click', (event) => {
+    if (event.target.classList.contains('popup__cross')) {
+        const popup = event.target.parentElement.parentElement;
+        popup.classList.remove('popup_is-opened')
+    }
+
 })
 
 
